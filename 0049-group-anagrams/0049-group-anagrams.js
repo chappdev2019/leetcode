c53589ee99820bf3c827e36ed6009094bddd2623
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-01-24 15:03:49 -0500

    Time: 49 ms (25.79%), Space: 63.5 MB (46.1%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -8,10 +8,9 @@ function groupAnagrams(strs) {
     for (let i = 0; i < strs.length; i++) {
         let counter = new Array(26).fill(0);
         strs[i].split('').forEach(e => counter[e.charCodeAt(0)-'a'.charCodeAt(0)]++)
-        let key = counter.join();
-        let val = map.get(key) || [];
-        val.push(strs[i]);
-        map.set(key, val);
+        let key = String(counter);
+        if (!map.has(key)) map.set(key, []);
+        map.get(key).push(strs[i]);
     }
-    return [...map.values()];
+    return Array.from(map.values());
 }
