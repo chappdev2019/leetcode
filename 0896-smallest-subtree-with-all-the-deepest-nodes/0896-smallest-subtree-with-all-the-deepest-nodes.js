8a9ec5941ff48ef49df6867377ba393497953fa0
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-01-25 21:18:32 -0500

    Time: 5 ms (3.45%), Space: 52 MB (16.09%) - LeetHub

--- 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.js
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.js
@@ -18,20 +18,33 @@ q = []         q2=[]
 
 [7,2][4,2]
 
-
-
+n* height of the tree
+n2
  */
 
+var subtreeWithAllDeepest = function(root) {
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+    if(left === right) return root;
+    if(left>right) return subtreeWithAllDeepest(root.left);
+    return subtreeWithAllDeepest(root.right);
+};
+var maxDepth = (root,depth=0)=>{
+    if(!root) return 0;
+    let height =1+ Math.max(maxDepth(root.left,depth+1),maxDepth(root.right,depth+1));
+    return height;
+}
 
 var subtreeWithAllDeepest = function(root) {
     if(!root) return root;
     let queue = new Queue();
     queue.enqueue([root]);
     let res = root;
+    let cache ={};
     while(queue.size()){
         let [node,depth] = queue.dequeue();
-        let left = maxDepth(node.left);
-        let right = maxDepth(node.right);
+        let left = maxDepth(node.left,cache);
+        let right = maxDepth(node.right,cache);
         if(left === right) return node;
         if(left>right) 
         queue.enqueue([node.left]);
@@ -40,22 +53,24 @@ var subtreeWithAllDeepest = function(root) {
     }
 
 };
-/*var maxDepth = (root,depth=0)=>{
+var maxDepth = (root,depth=0,memo={})=>{
     if(!root) return 0;
+    if(root in memo) return memo[root];
     let height =1+ Math.max(maxDepth(root.left,depth+1),maxDepth(root.right,depth+1));
+    memo[root]=height;
     return height;
-}*/
-var maxDepth = (root)=>{
-    if(!root) return 0;
-    let queue = new Queue();
-    queue.enqueue([root,1]);
-    let res = 0;
-    while(queue.size()){
-        let [node,depth] = queue.dequeue();
-        res=Math.max(res,depth);
-        if(node.left)queue.enqueue([node.left,depth+1]);
-        if(node.right)queue.enqueue([node.right,depth+1]);
-
-    }
-    return res;
 }
+// var maxDepth = (root)=>{
+//     if(!root) return 0;
+//     let queue = new Queue();
+//     queue.enqueue([root,1]);
+//     let res = 0;
+//     while(queue.size()){
+//         let [node,depth] = queue.dequeue();
+//         res=Math.max(res,depth);
+//         if(node.left)queue.enqueue([node.left,depth+1]);
+//         if(node.right)queue.enqueue([node.right,depth+1]);
+
+//     }
+//     return res;
+// }
