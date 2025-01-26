cf5da58fc09a3bea4084a6ddea10fb8fde72e128
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-01-26 00:03:36 -0500

    Time: 45 ms (57.33%), Space: 92.8 MB (80%) - LeetHub

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} startValue
+ * @param {number} destValue
+ * @return {string}
+ */
+var getDirections = function(root, startValue, destValue) {
+    let node = findCommonAncestor(root)
+    let cur = findStartValue(node);
+    let cur2 = findEndVal(node);
+    return cur+cur2;
+
+    function findCommonAncestor (root) {
+        if (root == null) return null;
+        if (root.val == startValue || root.val == destValue) return root;
+        let left = findCommonAncestor(root.left);
+        let right = findCommonAncestor(root.right);
+        if(right&&left) return root;
+        else if (right) return right;
+        return left;
+        
+    }
+
+    function findStartValue (root, path='') {
+        if (root == null) return null;
+        if (root.val == startValue) return path;
+        return (findStartValue(root.left, path+'U') ||
+        findStartValue(root.right, path+'U'));
+    }
+   function findEndVal (root, path='') {
+        if (root == null) return null;
+        if (root.val == destValue) return path;
+        return (findEndVal(root.left, path+'L') ||
+        findEndVal(root.right, path+'R'));
+    }
+};
