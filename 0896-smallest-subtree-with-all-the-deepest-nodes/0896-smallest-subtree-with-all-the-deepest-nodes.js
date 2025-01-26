182d5193e3e3f301d1378d18b3ad84f144bda542
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-01-25 21:11:51 -0500

    Time: 14 ms (3.45%), Space: 54.7 MB (6.9%) - LeetHub

--- 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.js
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.js
@@ -9,17 +9,53 @@
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
- 
+
+[root, level]
+q = []         q2=[]
+[3,0]
+[5,1] [1,1]
+
+
+[7,2][4,2]
+
+
+
  */
+
+
 var subtreeWithAllDeepest = function(root) {
-    let left = maxDepth(root.left);
-    let right = maxDepth(root.right);
-    if(left === right) return root;
-    if(left>right) return subtreeWithAllDeepest(root.left);
-    return subtreeWithAllDeepest(root.right);
+    if(!root) return root;
+    let queue = new Queue();
+    queue.enqueue([root]);
+    let res = root;
+    while(queue.size()){
+        let [node,depth] = queue.dequeue();
+        let left = maxDepth(node.left);
+        let right = maxDepth(node.right);
+        if(left === right) return node;
+        if(left>right) 
+        queue.enqueue([node.left]);
+        else 
+        queue.enqueue([node.right]);
+    }
+
 };
-var maxDepth = (root,depth=0)=>{
+/*var maxDepth = (root,depth=0)=>{
     if(!root) return 0;
     let height =1+ Math.max(maxDepth(root.left,depth+1),maxDepth(root.right,depth+1));
     return height;
+}*/
+var maxDepth = (root)=>{
+    if(!root) return 0;
+    let queue = new Queue();
+    queue.enqueue([root,1]);
+    let res = 0;
+    while(queue.size()){
+        let [node,depth] = queue.dequeue();
+        res=Math.max(res,depth);
+        if(node.left)queue.enqueue([node.left,depth+1]);
+        if(node.right)queue.enqueue([node.right,depth+1]);
+
+    }
+    return res;
 }
