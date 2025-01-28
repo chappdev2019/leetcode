b5bba892fe0a4fc2a024812fadb7a868844679ee
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-01-28 09:02:54 -0500

    Time: 64 ms (10.26%), Space: 50.5 MB (14.83%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.js
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.js
@@ -2,6 +2,7 @@
  * @param {string} word1
  * @param {string} word2
  * @return {string}
+ Time O(m+n) Space O(1)
  */
 var mergeAlternately = function(word1, word2) {
     let p1 = 0;
@@ -10,8 +11,10 @@ var mergeAlternately = function(word1, word2) {
     while (p1 < word1.length || p2 < word2.length) {
         res+= word1[p1]? word1[p1] : '';
         res+= word2[p2]? word2[p2] : '';
-        if (p1 < word1.length) p1++;
-        if (p2 < word2.length) p2++;
+        if (p1> word1.length) return res + word2.slice(p2+1);
+        if (p2> word2.length) return res + word1.slice(p1+1);
+        p1++;
+        p2++;
     }
     return res;
 };
