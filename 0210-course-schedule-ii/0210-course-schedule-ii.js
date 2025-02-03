9573469343c3a5fc0b3241c8d4c00811a8ef6379
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-03 08:10:11 -0500

    Time: 6 ms (90.45%), Space: 55 MB (79.36%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -4,7 +4,6 @@
  * @return {number[]}
  */
 var findOrder = function(numCourses, prerequisites) {
-
     let map = new Map();
     let indegree = new Array(numCourses).fill(0);
     for (let [course,prev] of prerequisites) {
@@ -12,20 +11,19 @@ var findOrder = function(numCourses, prerequisites) {
         map.get(prev).push(course);
         indegree[course]++;
     }
-   
     let stack = [];
     for(let i = 0; i < indegree.length; i++) {
         if(indegree[i]==0) stack.push(i);
     }
-    let visited = new Set();
+    let visited = [];
     while(stack.length > 0) {
         let cur = stack.pop();
         let list = map.get(cur) || []
-        visited.add(cur);
+        visited.push(cur);
         for (let course of list) {
             indegree[course]--;
             if (indegree[course] == 0) stack.push(course);
         }
     } 
-    return visited.size == numCourses ? [...visited] : [];
+    return visited.length == numCourses ? visited : [];
 };
