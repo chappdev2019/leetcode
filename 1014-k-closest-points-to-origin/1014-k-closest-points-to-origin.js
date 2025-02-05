5c47db49d8ccab1c110244c4b1929676eed191c9
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-04 19:07:44 -0500

    Time: 19 ms (93.84%), Space: 61.3 MB (97.68%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.js
@@ -3,13 +3,45 @@
  * @param {number} k
  * @return {number[][]}
  */
-var kClosest = function(points, k) {
-    let arear = new Array(points.length).fill(0);
-    let pos = 0;
-    for (const point of points) {
-        let [x,y] = point;
-        arear[pos++] = [x**2 + y**2, point];
+var kClosest = function (points, k) {
+
+    const result = quickSelect();
+    return result;
+
+    function quickSelect() {
+        let right = points.length-1; 
+        let left = 0;
+        let pivotIndex = points.length;
+        while (pivotIndex !== k) { 
+            pivotIndex = partition(points, left, right)
+            if (pivotIndex < k) {
+                left = pivotIndex
+            } else {
+                right = pivotIndex - 1;
+            }
+        }
+        return points.slice(0, k);
     }
-    arear.sort((a,b)=> a[0] - b[0]);
-    return arear.slice(0,k).map(e => e[1]);
+
+    function partition(points, left, right) {
+        let pivot = points[Math.floor((right + left) / 2)];
+        let pivotArea = calArea(pivot);
+        while (left < right) {
+            if (calArea(points[left]) >= pivotArea) {
+                [points[left], points[right]] = [points[right], points[left]];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        if (calArea(points[left]) < pivotArea) {
+            left++;
+        }
+        return left;
+    }
+
+    function calArea (point) {
+        const [x,y] = point;
+        return x**2+ y**2
+    }
 };
