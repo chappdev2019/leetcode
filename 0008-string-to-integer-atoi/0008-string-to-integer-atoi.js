56f3ad09699f1ae3757f9de7ac5a90a9b8be4715
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-05 21:32:31 -0500

    Time: 6 ms (16.39%), Space: 54 MB (43.7%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (s) {
+    let start = 0;
+    s = s.trim();
+    let cur = 0;
+    let sign = null;
+    while (start < s.length) {
+        if (start == 0 && (s[start] == '-' || s[start] == '+')) {
+            sign = s[start];
+            start++;
+        }
+        while (!isNaN(s[start])&& s[start]!=' ' && s[start]!='  ') {
+            if (cur == 0 && s[start] == 0) {
+                start++;
+            } else {
+                cur = cur * 10 + Number(s[start]);
+                start++;
+            }
+        }
+        if(sign == '-') cur *= -1;
+
+        let max = 2**31 - 1;
+        let min = -(2**31);
+
+        if (cur > max || cur < min) {
+           cur = cur > max ? max : min;
+        }
+        return cur;
+    }
+    return 0;
+};
