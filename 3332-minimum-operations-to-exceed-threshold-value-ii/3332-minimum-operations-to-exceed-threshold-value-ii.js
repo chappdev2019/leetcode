62542bb217b6e3b3dbb2df8a1670c113e94db31e
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-14 14:43:03 -0500

    Time: 112 ms (85.68%), Space: 73.5 MB (83.61%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.js
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.js
@@ -6,9 +6,10 @@
 var minOperations = function(nums, k) {
     nums.sort((a, b) => b- a)
     const getSmallest = () => {
-        if(nums[nums.length-1] && !(nums[nums.length-1] > arr[arrPos])) return nums.pop()
+        if(nums[nums.length-1] && (arr[arrPos] == undefined || nums[nums.length-1] <= arr[arrPos])) return nums.pop()
         return arr[arrPos++]
     };
+    
     let arr = [], operTime = 0, arrPos = 0
     while((nums.length > 0 && nums[nums.length-1] < k) || (arr.length > 0 && arr[arrPos] < k)) {
         let min1 = getSmallest();       
