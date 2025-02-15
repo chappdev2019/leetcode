0515c11bed5bd726e18198bac797525787bb8b4c
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-15 16:41:15 -0500

    Time: 0 ms (100%), Space: 52.2 MB (16.67%) - LeetHub

---
+++ 3221-find-the-peaks/3221-find-the-peaks.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} mountain
+ * @return {number[]}
+ */
+var findPeaks = function(mountain) {
+    let res = [];
+    for (let i = 1; i < mountain.length-1; i++) {
+        if (mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1]) res.push(i)
+    }
+    return res;
+};
