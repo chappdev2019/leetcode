66136205164594d71c0b1cec79fbf1a84b8a1bd1
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-15 22:41:32 -0500

    Time: 1 ms (100%), Space: 56.6 MB (29.23%) - LeetHub

---
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSubPath = function(head, root) {
+    if (root== null) return false;
+    return dfs(head, root) || isSubPath(head, root.left) ||isSubPath(head, root.right);
+    function dfs(head, root) {
+        if (head === null) return true;
+        if (root === null) return false;
+        if (head.val != root.val) return false;
+        // if (head.val === root.val) {
+            return dfs(head.next, root.left) ||dfs(head.next, root.right) 
+        // }
+    }
+};
