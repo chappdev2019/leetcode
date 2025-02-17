09d806e2ac3d8d4224bd378dbd553a007a624538
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-16 22:52:04 -0500

    Time: 1 ms (38.97%), Space: 49.1 MB (82.41%) - LeetHub

---
+++ 0851-goat-latin/0851-goat-latin.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function(sentence) {
+    let res = [];
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    let words = sentence.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        let cur = words[i];
+        let word = vowels.has(cur[0]) ? cur : cur.slice(1) + cur[0];
+        word += 'ma' + 'a'.repeat(i+1);
+        res.push(word);
+    }
+    return res.join(' ');
+};
