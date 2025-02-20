94a8205cbd5f9e284dd42fbd79f1b4f60a78f39f
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-20 13:44:46 -0500

    Time: 57 ms (80.49%), Space: 62.6 MB (5.69%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.js
@@ -0,0 +1,37 @@
+var removeInvalidParentheses = function(s) {
+    let res = [];
+    let queue = new Set([s]);
+    let found = false;
+    while (queue.size) {
+        let nextQueue = new Set();
+        for (let str of queue) {
+            if (isValid(str)) {
+                res.push(str);
+                found = true;
+            }
+            if (!found) {
+                for (let i = 0; i < str.length; i++) {
+                    if (str[i] !== "(" && str[i] !== ")") continue;
+                    let newStr = str.slice(0, i) + str.slice(i + 1);
+                    nextQueue.add(newStr);
+                }
+            }
+        }
+        if (found) {
+            break
+        }
+        queue = nextQueue;
+
+    }
+    return res.length ? res : [""];
+};
+
+function isValid(s) {
+    let count = 0;
+    for (let char of s) {
+        if (char === "(") count++;
+        else if (char === ")") count--;
+        if (count < 0) return false;
+    }
+    return count === 0;
+}
