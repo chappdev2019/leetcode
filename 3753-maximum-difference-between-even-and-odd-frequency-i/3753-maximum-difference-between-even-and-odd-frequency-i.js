bd7a9b4c3f426069f1da6b0a9dcc5380e6446a9b
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-20 19:16:50 -0500

    Time: 5 ms (70.77%), Space: 56.7 MB (6.69%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.js
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.js
@@ -2,26 +2,20 @@
  * @param {string} s
  * @return {number}
  */
-var maxDifference = function(s) {
+var maxDifference = function (s) {
     let freq = new Array(26).fill(0);
     for (const char of s) {
-        freq[char.charCodeAt(0)-'a'.charCodeAt(0)]++;
+        freq[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
     }
-    let filtered = freq.filter(e => e!=0).sort((a,b)=>b-a);
-    let maxOdd = 0;
-    let maxEven = 0;
-    for (let i = 0; i < filtered.length; i++) {
-        if (filtered[i]%2!=0) {
-            maxOdd = filtered[i]
-            break;
-        } 
-    }
-    for (let i = filtered.length-1; i>=0; i--) {
-        if (filtered[i]%2==0) {
-            maxEven = filtered[i]
-            break;
+    let maxOdd = 0, minEven = Infinity;
+    for (const count of freq) {
+        if (count === 0) continue;
+        if (count % 2 === 0) {
+            minEven = Math.min(minEven, count);
+        } else {
+            maxOdd = Math.max(maxOdd, count);
         }
-
     }
-    return maxOdd-maxEven;
+
+    return maxOdd - minEven;
 };
