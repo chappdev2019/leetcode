e928f09b84b9adb1503e4ff634bc2c70337a6922
cguo_dev <59427068+chappdev2019@users.noreply.github.com>
2025-02-20 20:09:17 -0500

    Time: 5 ms (29.32%), Space: 57.7 MB (5.22%) - LeetHub

---
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var countPrefixSuffixPairs = function(words) {
+    let count = 0;
+    const isPrefixAndSuffix = (word1, word2) => {
+        const wordOneLen = word1.length;
+        const wordTwoLen = word2.length;
+        const isPrefix = () => word1 === word2.slice(0, wordOneLen);
+        const isSuffix = () => word1 === word2.slice(wordTwoLen-wordOneLen);
+        if (isPrefix() && isSuffix())  return true;
+        return false;
+    }
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i+1; j < words.length; j++) {
+            if(isPrefixAndSuffix(words[i],words[j])) count++;
+        }
+    }
+    return count;
+};
