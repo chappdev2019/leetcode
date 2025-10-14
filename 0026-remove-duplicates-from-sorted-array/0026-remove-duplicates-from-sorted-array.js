6545e023621df8b70bb808e53b8e0f5a4fbe3fa7
Christy Guo <59427068+chappdev2019@users.noreply.github.com>
2025-10-14 14:57:35 -0400

    Time: 0 ms (100%), Space: 57.3 MB (73.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -7,7 +7,7 @@ var removeDuplicates = function(nums) {
     let slow = 1;
     while (fast < nums.length) {
         if (nums[fast] != nums[slow-1]) {
-            [nums[fast], nums[slow]] = [nums[slow], nums[fast]];
+            nums[slow] = nums[fast];
             slow++;
         } 
         fast++;
