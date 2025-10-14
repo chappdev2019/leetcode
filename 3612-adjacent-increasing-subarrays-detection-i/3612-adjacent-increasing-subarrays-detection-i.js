eba38a90a4e361f965626ffafbbb1db99c422b41
Christy Guo <59427068+chappdev2019@users.noreply.github.com>
2025-10-14 11:29:57 -0400

    Time: 63 ms (46.43%), Space: 56 MB (96.43%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.js
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.js
@@ -4,33 +4,46 @@
  * @return {boolean}
  */
 var hasIncreasingSubarrays = function(nums, k) {
-  const n = nums.length;
+//   const n = nums.length;
 
-  for (let start = 0; start <= n - 2 * k; start++) {
-    let firstIncCount = 1;
+//   for (let start = 0; start <= n - 2 * k; start++) {
+//     let firstIncCount = 1;
 
-    for (let i = start + 1; i < start + k; i++) {
-      if (nums[i] > nums[i - 1]) {
-        firstIncCount++;
-      } else {
-        break;
-      }
-    }
+//     for (let i = start + 1; i < start + k; i++) {
+//       if (nums[i] > nums[i - 1]) {
+//         firstIncCount++;
+//       } else {
+//         break;
+//       }
+//     }
 
-    if (firstIncCount === k ) {
-      let secondIncCount = 1;
+//     if (firstIncCount === k ) {
+//       let secondIncCount = 1;
 
-      for (let i = start + k + 1; i < start + 2 * k; i++) {
-        if (nums[i] > nums[i - 1]) {
-          secondIncCount++;
-        } else {
-          break;
+//       for (let i = start + k + 1; i < start + 2 * k; i++) {
+//         if (nums[i] > nums[i - 1]) {
+//           secondIncCount++;
+//         } else {
+//           break;
+//         }
+//       }
+
+//       if (secondIncCount === k) return true;
+//     }
+//   }
+
+//   return false;
+
+    let prevCount, currCount = 1
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] < nums[i + 1]) currCount++
+        else {
+            prevCount = currCount
+            currCount = 1
         }
-      }
 
-      if (secondIncCount === k) return true;
+        if (currCount >= 2 * k || (prevCount >= k && currCount >= k)) return true
     }
-  }
 
-  return false;
+    return false
 };
