0847d110440617cdf881dbe2449239408f99153e
Christy Guo <59427068+chappdev2019@users.noreply.github.com>
2025-10-14 23:35:35 -0400

    Time: 5 ms (28.2%), Space: 67.4 MB (16.78%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -5,7 +5,18 @@
  */
 var rotate = function(nums, k) {
     k = k % nums.length;
-    if (k == 0 ) return;
-    let rotated = nums.slice(-k).concat(nums.slice(0, nums.length - k));
-    nums.splice(0, nums.length,...rotated);
+    // if (k == 0 ) return;
+    // let rotated = nums.slice(-k).concat(nums.slice(0, nums.length - k));
+    // nums.splice(0, nums.length,...rotated);
+    const rotate = (start, end) => {
+        while(start < end) {
+            [nums[start], nums[end]] = [nums[end], nums[start]];
+            start++;
+            end--;
+        }
+    }
+    rotate(0, nums.length-1);
+    rotate(0, k-1);
+    rotate(k, nums.length-1);
+
 };
