d77e7d602b5a402ee00b5e028f213dc7f7963e8b
Christy Guo <59427068+chappdev2019@users.noreply.github.com>
2025-10-17 21:50:53 -0400

    Time: 108 ms (14.08%), Space: 58.9 MB (62.59%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -5,13 +5,20 @@
  3  3  3  x  
 
  */
-var canJump = function (nums) {
-    let maxDestination = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (maxDestination < i) {
-            return false;
+var canJump = function(nums) {
+    const n = nums.length;
+    let canReachEnd = new Array(n).fill(false);
+    canReachEnd[n - 1] = true; 
+
+    for (let i = n - 2; i >= 0; i--) {
+        let furthestJump = Math.min(i + nums[i], n - 1);
+        for (let j = i + 1; j <= furthestJump; j++) {
+            if (canReachEnd[j]) {
+                canReachEnd[i] = true;
+                break;
+            }
         }
-        maxDestination = Math.max(nums[i] + i, maxDestination);
     }
-    return true;
+
+    return canReachEnd[0];
 };
