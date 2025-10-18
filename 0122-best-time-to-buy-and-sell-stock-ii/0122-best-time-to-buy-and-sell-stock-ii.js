3afa684c9494ce3311eff2da35e7bb1feb5ca296
Christy Guo <59427068+chappdev2019@users.noreply.github.com>
2025-10-17 21:09:59 -0400

    Time: 6 ms (6.9%), Space: 61.1 MB (5.04%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    const len = prices.length;
+    const dp = Array.from({ length: len }, () => [0, 0]);
+    dp[0][0] = 0;
+    dp[0][1] = -prices[0];
+    for (let i = 1; i < len; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+    }
+    return dp[len - 1][0];
+};
