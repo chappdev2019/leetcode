9d6df7154c3fcee10e9eadfe7509c137307a3365
Christy Guo <59427068+chappdev2019@users.noreply.github.com>
2025-10-24 00:16:18 -0400

    Time: 18 ms (7.14%), Space: 61.6 MB (7.14%) - LeetHub

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var hasSameDigits = function (s) {
+    let cur = s.split('');
+    while (cur.length > 2) {
+        let next = [];
+        for (let i = 0; i + 1 < cur.length; i++) {
+            next.push((Number(cur[i]) + Number(cur[i + 1])) % 10);
+        }
+        cur = next;
+    }
+    console.log(cur)
+    return cur[0] === cur[1];
+};
