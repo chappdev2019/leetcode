93dd531e361ff12822834af89a03866b43c527a5
Christy Guo <59427068+chappdev2019@users.noreply.github.com>
2025-10-25 22:51:03 -0400

    Time: 30 ms (59.09%), Space: 88.7 MB (51.95%) - LeetHub

---
+++ 2169-simple-bank-system/2169-simple-bank-system.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} balance
+ */
+var Bank = function(balance) {
+    this.balance = balance;
+    console.log(this.balance)
+};
+
+/** 
+ * @param {number} account1 
+ * @param {number} account2 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.transfer = function(account1, account2, money) {
+    if (!this.balance || this.balance[account1-1] < money || account1 > this.balance.length || account2 > this.balance.length) return false;
+    this.balance[account1-1] -= money;
+    this.balance[account2-1] += money;
+    return true;
+};
+
+/** 
+ * @param {number} account 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.deposit = function(account, money) {
+    if (!this.balance || account > this.balance.length) return false;
+    this.balance[account-1] += money;
+    return true;
+};
+
+/** 
+ * @param {number} account 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.withdraw = function(account, money) {
+    if (!this.balance || account > this.balance.length || this.balance[account-1] < money) return false;
+    this.balance[account-1] -= money;
+    return true;
+};
+
+/** 
+ * Your Bank object will be instantiated and called as such:
+ * var obj = new Bank(balance)
+ * var param_1 = obj.transfer(account1,account2,money)
+ * var param_2 = obj.deposit(account,money)
+ * var param_3 = obj.withdraw(account,money)
+ */
