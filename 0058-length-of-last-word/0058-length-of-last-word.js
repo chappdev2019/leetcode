60d4eae4712d7237482340a5dbb8041bbbac7951
Christy Guo <59427068+chappdev2019@users.noreply.github.com>
2025-10-26 19:43:20 -0400

    Time: 0 ms (100%), Space: 54.6 MB (21.34%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let start = s.length - 1;
-    let r = s.split(' ').filter((e) => e.length > 0);
+    const r =  s.trim().split(' ');
     return r[r.length-1].length
 };
